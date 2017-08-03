27b2d37dd27891c4445faf66babbe942a5c9bc51
dankato <apearmor@gmail.com>
2017-08-03 12:16:05 -0700

    reducer tests are operational

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -16,13 +16,32 @@ describe('reducer', () => {
     expect(state.correctAnswer).toEqual(value);
     expect(state.showInfoModal).toEqual(false);
     })
+
+    // this is where we left off, this is passing but need expect
+    console.log('start up here');
+
     it('Should provide feedback and add guess to state on MakeGuess', ()=> {
-        const value = 45;
+        const value = 100;
         const state = {
             guesses: [],
-            feedback: 'Make your guess!',
+            feedback: '',
             correctAnswer: value,
             showInfoModal: false
         };
+        let newState = reducer(state, makeGuess(10));
+        expect(newState.guesses).toEqual([10]);
+        expect(newState.feedback).toEqual('You\'re Ice Cold...');
+
+        newState = reducer(state, makeGuess(71));
+        expect(newState.guesses).toEqual([71]);
+        expect(newState.feedback).toEqual('You\'re Warm');
+
+        newState = reducer(state, makeGuess(99));
+        expect(newState.guesses).toEqual([99]);
+        expect(newState.feedback).toEqual('You\'re Hot!');
+
+        newState = reducer(state, makeGuess(100));
+        expect(newState.guesses).toEqual([100]);
+        expect(newState.feedback).toEqual('You got it!');
     })
 })
